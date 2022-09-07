b2484ce4141f2d1ce2f79aa5fddf0501e7672836
georgina <georginaxpuig@gmail.com>
2022-09-07 16:28:55 -0300

    add map and flat

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Flat from './components/Flat'
+import SimpleMap from './components/GoogleMap'
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
+const Hello = () => {
   return (
     <div>
-      Hello,
-      {name}
+      <div className="flat-list">
+        <Flat />
+      </div>
+      
+      <div className="map-container">
+        <SimpleMap />
+      </div>
     </div>
   );
 };
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello />, root);
 }
